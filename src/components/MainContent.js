4b479af51114f1de31ca937835d9201e06aa9d7c
usman <usman5501798@gmail.com>
2023-09-25 13:08:27 +0500

    layout changed

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,8 +1,8 @@
-import { Image, Layout, Select, Space } from 'antd';
-import { useState } from 'react';
+import { Image, Layout, Space } from 'antd';
+
 import SearchMember from './SearchMember';
 
-const { Option } = Select;
+
 const { Content } = Layout;
 
 const contentStyle = {
@@ -11,64 +11,8 @@ const contentStyle = {
   backgroundColor: 'transparent',
 };
 
-const languageOptions = [
-  'English',
-  'Spanish',
-  'French',
-  'German',
-  'Italian',
-  'Portuguese',
-  'Dutch',
-  'Russian',
-  'Chinese',
-  'Japanese',
-  'Korean',
-  'Arabic',
-  'Turkish',
-  'Swedish',
-  'Danish',
-  'Norwegian',
-  'Finnish',
-  'Greek',
-  'Hindi',
-  'Bengali',
-  'Urdu',
-  'Punjabi',
-  'Vietnamese',
-  'Thai',
-  'Hebrew',
-  'Polish',
-  'Romanian',
-  'Czech',
-  'Hungarian',
-  'Ukrainian',
-  'Slovak',
-  'Slovenian',
-  'Croatian',
-  'Serbian',
-  'Bulgarian',
-  'Estonian',
-  'Latvian',
-  'Lithuanian',
-  'Macedonian',
-  'Albanian',
-  'Georgian',
-  'Armenian',
-  'Azerbaijani',
-  'Kazakh',
-  'Uzbek',
-];
-
 const MainContent = () => {
-  // Declare and initialize the state variable
-  const [isLanguageSelected, setIsLanguageSelected] = useState(false);
-  const [selectedLangauge, setLanguageSelected] = useState("Choose Language")
 
-  let handleLanguageChange = (language) => {
-    console.log(language)
-    setLanguageSelected(language)
-    setIsLanguageSelected(true)
-  }
 
   return (
     <Space
@@ -84,26 +28,8 @@ const MainContent = () => {
           <Image src={require('../Banner.jpg')} height={'100%'} style={{ marginTop: '50px' }} />
           <h1 style={{fontSize: '50px', marginTop: '40px'}}>CAMPSIE RSL PROMOTION</h1>
           <p style={{fontSize: '30px', fontWeight: '400', marginTop: '5px'}}>PUTTING OUR COMMUNITY FIRST</p>
-          {/* Show Language Component If Language is not selected */}
-          {isLanguageSelected == false ? <Select
-            showSearch
-            size='medium'
-            style={{ width: '400px', marginTop: '10px'}}
-            placeholder="Choose Language"
-            optionFilterProp="children"
-            onChange={handleLanguageChange}
-            value={selectedLangauge}
-            filterOption={(input, option) =>
-              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-            }
-          >
-            {languageOptions.map((language, index) => (
-              <Option key={index} value={language}>
-                {language}
-              </Option>
-            ))}
-          </Select> : 
-          <SearchMember/> }
+          {/* Show Language Component If Language is not selected */} 
+          <SearchMember/> 
         </Content>
       </Layout>
     </Space>
--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,10 +1,60 @@
 import React from 'react';
-import { Image, Layout, Space } from 'antd';
-
+import { Image, Layout, Space, Select } from 'antd';
+import {useState} from 'react';
+const { Option } = Select;
 const { Header } = Layout;
+const languageOptions = [
+  'English',
+  'Spanish',
+  'French',
+  'German',
+  'Italian',
+  'Portuguese',
+  'Dutch',
+  'Russian',
+  'Chinese',
+  'Japanese',
+  'Korean',
+  'Arabic',
+  'Turkish',
+  'Swedish',
+  'Danish',
+  'Norwegian',
+  'Finnish',
+  'Greek',
+  'Hindi',
+  'Bengali',
+  'Urdu',
+  'Punjabi',
+  'Vietnamese',
+  'Thai',
+  'Hebrew',
+  'Polish',
+  'Romanian',
+  'Czech',
+  'Hungarian',
+  'Ukrainian',
+  'Slovak',
+  'Slovenian',
+  'Croatian',
+  'Serbian',
+  'Bulgarian',
+  'Estonian',
+  'Latvian',
+  'Lithuanian',
+  'Macedonian',
+  'Albanian',
+  'Georgian',
+  'Armenian',
+  'Azerbaijani',
+  'Kazakh',
+  'Uzbek',
+];
 
 const headerStyle = {
-  textAlign: 'start',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
   color: '#fff',
   height: 100,
   paddingInline: 50,
@@ -12,14 +62,40 @@ const headerStyle = {
   backgroundColor: '#0b1e33',
 };
 
-const MainHeader = () => (
+const MainHeader = () => {
+  const [selectedLangauge, setLanguageSelected] = useState("Choose Language")
+
+  let handleLanguageChange = (language) => {
+    console.log(language)
+    setLanguageSelected(language)
+  }
+  return(
     <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
       <Layout>
         <Header style={headerStyle}>
             <Image src={require('../assets/images/home.jpg')} height={100} />
+            <Select
+            showSearch
+            size='medium'
+            style={{ width: '100px' , height: '30px', marginTop: '10px'}}
+            // placeholder="Choose Language"
+            title='Choose Language'
+            optionFilterProp="children"
+            onChange={handleLanguageChange}
+            value={selectedLangauge}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            {languageOptions.map((language, index) => (
+              <Option key={index} value={language}>
+                {language}
+              </Option>
+            ))}
+          </Select>
         </Header>
       </Layout>
     </Space>
-  );
+)};
 
 export default MainHeader;
