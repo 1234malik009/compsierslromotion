fa09669c87cdc026b3b429d009214120ad601467
usman <usman5501798@gmail.com>
2023-09-28 11:34:27 +0500

    changes

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,15 +1,17 @@
-import { Input , Card, Form, Typography, Button, InputNumber } from "antd"
+import { Input , Card, Form, Typography, Button, InputNumber, Row } from "antd"
 
 
 
-const UpdateProfile = ({visible,showOnClick}) => {
+const UpdateProfile = () => {
   return (
     <div>
 
     
 
     <Card style={{maxWidth: '750px', marginInline: 'auto'}} bordered={false}>
-    <Form>
+    <Form
+    >
+
     <Typography.Title className={'text-play'}
     style={{textAlign: 'left'}}>Update Your Profile
     </Typography.Title>
@@ -20,30 +22,53 @@ const UpdateProfile = ({visible,showOnClick}) => {
    
    </div>
    <div className="parent">
+   <Row>
+    
+   <Form.Item 
+    name='firstName'
+    rules={[{ required: true, message: 'Please input your first name!' }]}
+    >
       
       <Input size="large"  placeholder="First Name" className=" item-feild input-text" />
+      </Form.Item>
+   </Row>
+    <Row>
+
+      <Form.Item
+      name='lastName'
+      rules={[{ required: true, message: 'Please input your last name!' }]}
+    >
       
       <Input size="large" placeholder="Last Name" className=" item-feild input-text" />
+      </Form.Item>
+    </Row>
    </div>
    <div style={{textAlign:'start'}}>
 
 
     
       <Typography.Text className='text-white ' style={{color: "white", fontSize: "16px"}}>Phone No</Typography.Text>
+      <Form.Item
+      name='phoneNo'
+      rules={[{ required: true, message: 'Please input your phone number!' }]}>
+      
       <InputNumber size="large" className={'ant-input '}  placeholder="Phone No" type="number"/>
+      </Form.Item>
       <Typography.Text className='text-white ' style={{color: "white", fontSize: "16px"}}>Email</Typography.Text>
+      <Form.Item
+      name='Email'
+      rules={[{ required: true, message: 'Please input your email!' }]}>
       <Input size="large" className={'ant-input '}  placeholder="Email" type="email"/>
+      </Form.Item>
       <Button className={'btn'}
        size={'large'}
        type="primary"
        htmlType="submit"
        style={{marginTop: "10px"}}
-       onClick={showOnClick}
     
        >
       Update Profile
       </Button>
-      {visible && <UpdateProfile/>}
    </div>
     </Form>
     </Card>
