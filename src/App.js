5f0b587d8e163ab8c5522404dab18e2ba818ac5f
ma9241412 <ma9241412@gmail.com>
2023-10-18 09:37:08 -0700

    chnages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import './App.css';
-import MainContent from './components/MainContent';
-import MainHeader from './components/MainHeader';
-import GoogleTranslater from "./components/GoogleTranslator";
+import React from 'react'
+import './App.css'
+import MainContent from './components/MainContent'
+import MainHeader from './components/MainHeader'
+// import GoogleTranslater from './components/GoogleTranslator'
+import { Route, Routes } from 'react-router-dom'
+import TokenPage from './components/TokenPage'
+import TokenExpiredPage from './components/TokenExpiredPage'
 
 function App() {
-    return (
-            <div className='body'>
-                <MainHeader/>
-                <div style={{position: 'absolute', right: '10px', top: '-20px'}}>
-                    <GoogleTranslater/>
-                </div>
-                <MainContent/>
-            </div>
-
-    );
+	return (
+		<div className="body">
+			<MainHeader />
+			{/* <div style={{ position: 'absolute', right: '10px', top: '-20px' }}>
+				<GoogleTranslater />
+			</div> */}
+			<Routes>
+				<Route path="/" element={<MainContent />} />
+				<Route path="/:token" element={<TokenPage />} />
+				<Route path="/token-expired" element={<TokenExpiredPage />} />
+			</Routes>
+		</div>
+	)
 }
 
-export default App;
+export default App
