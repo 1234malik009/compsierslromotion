54e9b067ec537e820c7851228ac01e51d106acc0
Danyal Mehmood <adanyal46@gamil.com>
2024-01-17 12:55:40 +0500

    update profile set form values

--- src/Pages/UpdateProfile.js
+++ src/Pages/UpdateProfile.js
@@ -1,26 +1,31 @@
 import { Input, Card, Form, Typography, Button, InputNumber} from "antd";
 import {startSession} from "../helper/api";
+import { useEffect } from "react";
 
-const UpdateProfile = ({ setCompsieCompState }) => {
-
+const UpdateProfile = ({ setCompsieCompState, memberData }) => {
+  let [form] = Form.useForm();
+  useEffect(() => {
+    if (memberData) {
+      form.setFieldsValue({
+        firstName: memberData.firstName,
+        lastName: memberData.lastName,
+        phoneNo: memberData.mobilePhone,
+        Email: memberData.Email,
+      });
+    }
+  }, [form]);
   let updateProfile = async (data) => {
-    console.log(data)
-    const {sessionToken} = await startSession();
-    console.log(sessionToken)
-    setCompsieCompState(0)
-  }
+    console.log(data);
+    const { sessionToken } = await startSession();
+    console.log(sessionToken);
+    setCompsieCompState(0);
+  };
 
   return (
     <div>
-      <Card
-        style={{ maxWidth: "750px", marginInline: "auto" }}
-        bordered={false}
-      >
-        <Form onFinish={updateProfile} layout="vertical">
-          <Typography.Title
-            className={"text-play"}
-            style={{ textAlign: "left", fontSize: '30px'}}
-          >
+      <Card style={{ maxWidth: "750px", marginInline: "auto" }} bordered={false}>
+        <Form form={form} onFinish={updateProfile} layout="vertical">
+          <Typography.Title className={"text-play"} style={{ textAlign: "left", fontSize: "30px" }}>
             Update Your Profile
           </Typography.Title>
           <div style={{ textAlign: "start" }}>
@@ -29,48 +34,28 @@ const UpdateProfile = ({ setCompsieCompState }) => {
               name="firstName"
               rules={[{ required: true, message: "Please input your first name!" }]}
             >
-              <Input
-                size="medium"
-                className="w-100"
-                placeholder="First Name"
-              />
+              <Input size="medium" className="w-100" placeholder="First Name" />
             </Form.Item>
             <Form.Item
               label="Last Name"
               name="lastName"
               rules={[{ required: true, message: "Please input your last name!" }]}
             >
-              <Input
-                size="medium"
-                className="w-100"
-                placeholder="Last Name"
-              />
+              <Input size="medium" className="w-100" placeholder="Last Name" />
             </Form.Item>
             <Form.Item
               label="Mobile No"
               name="phoneNo"
-              rules={[
-                { required: true, message: "Please input your mobile number!" },
-              ]}
+              rules={[{ required: true, message: "Please input your mobile number!" }]}
             >
-              <InputNumber
-                size="medium"
-                className="w-100"
-                placeholder="Mobile No"
-                type="number"
-              />
+              <InputNumber size="medium" className="w-100" placeholder="Mobile No" type="number" />
             </Form.Item>
             <Form.Item
               label={"Email Address"}
               name="Email"
               rules={[{ required: true, message: "Please input your email!" }]}
             >
-              <Input
-                size="medium"
-                style={{width: '100%'}}
-                placeholder="Email"
-                type="email"
-              />
+              <Input size="medium" style={{ width: "100%" }} placeholder="Email" type="email" />
             </Form.Item>
 
             <Button
--- src/Theme/MainContent.js
+++ src/Theme/MainContent.js
@@ -17,46 +17,62 @@ const MainContent = () => {
     const [memberData, setMemberData] = useState([])
 
     return (
-        <Space
-            direction="vertical"
-            style={{
-                width: '100%',
-                height: '100%',
-            }}
-            size={[0, 48]}
-        >
-            <Layout>
-                <Content style={contentStyle}>
-                    {compsieCompState === 0 && <Image
-                        className="banner-image"
-                        src={require('../main.jpg')}
-                        style={{width: '95%', height: '330px', marginTop: '50px'}}
-                    />}
+      <Space
+        direction="vertical"
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        size={[0, 48]}
+      >
+        <Layout>
+          <Content style={contentStyle}>
+            {compsieCompState === 0 && (
+              <Image
+                className="banner-image"
+                src={require("../main.jpg")}
+                style={{ width: "95%", height: "330px", marginTop: "50px" }}
+              />
+            )}
 
-                    <Typography.Title level={1} className={'hero__title text-play'}>
-                        CAMPSIE RSL
-                    </Typography.Title>
-                    <Typography.Text className={'text-inter hero__para'}>
-                        PUTTING OUR COMMUNITY FIRST
-                    </Typography.Text>
-                    <br/>
+            <Typography.Title level={1} className={"hero__title text-play"}>
+              CAMPSIE RSL
+            </Typography.Title>
+            <Typography.Text className={"text-inter hero__para"}>
+              PUTTING OUR COMMUNITY FIRST
+            </Typography.Text>
+            <br />
 
-                    {compsieCompState === 0 ? (
-                        <Button onClick={() => setCompsieCompState(1)}
-                                style={{marginTop: '10px', maxWidth: '250px',width: '100%', height: '42px', color: 'white'}} type={"primary"}
-                                size={'large'} className={'btn'}>Enter</Button>
-                    ) : compsieCompState === 1 ? (
-                        <SearchMember
-                            setCompsieCompState={setCompsieCompState}
-                            setMemberData={setMemberData}
-                        />
-                    ) : compsieCompState === 2 ? (
-                        <MemberDetail memberData={memberData} setCompsieCompState={setCompsieCompState}/>
-                    ) : <UpdateProfile setCompsieCompState={setCompsieCompState}/>}
-                </Content>
-            </Layout>
-        </Space>
-    )
+            {compsieCompState === 0 ? (
+              <Button
+                onClick={() => setCompsieCompState(1)}
+                style={{
+                  marginTop: "10px",
+                  maxWidth: "250px",
+                  width: "100%",
+                  height: "42px",
+                  color: "white",
+                }}
+                type={"primary"}
+                size={"large"}
+                className={"btn"}
+              >
+                Enter
+              </Button>
+            ) : compsieCompState === 1 ? (
+              <SearchMember
+                setCompsieCompState={setCompsieCompState}
+                setMemberData={setMemberData}
+              />
+            ) : compsieCompState === 2 ? (
+              <MemberDetail memberData={memberData} setCompsieCompState={setCompsieCompState} />
+            ) : (
+              <UpdateProfile memberData={memberData} setCompsieCompState={setCompsieCompState} />
+            )}
+          </Content>
+        </Layout>
+      </Space>
+    );
 }
 
 export default MainContent
